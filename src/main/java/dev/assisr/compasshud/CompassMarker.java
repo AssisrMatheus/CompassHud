e561532e2891e328b605a0dfab408c697aa5f142
AssisrMatheus <assisr.matheus@gmail.com>
2026-06-27 01:25:15 +0000

    [version 1]

--- src/main/java/dev/assisr/compasshud/CompassMarker.java
+++
@@ -1,6 +0,0 @@
-package dev.assisr.compasshud;
-
-import net.minecraft.network.chat.TextColor;
-
-public record CompassMarker(int bearing, String label, TextColor color, char glyph, boolean clampToEdge) {
-}
